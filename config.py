150cb96ee43ca25c21d5c09867d6eaddee98f4eb
Alexander <sashakaradyaur345@gmail.com>
2020-07-20 19:24:04 +0300

    Integrate OOP to file

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-profesion = ['Пожарный','Полицейский','Военный','Хирург','Медсестра','Химик','Физик','Астро-физик']
+import random
+
+class Сharacter:
+    def __init__(self)
+        self.age = random.randint(15,80)
+        self.profesion = random.choice(['Пожарный',
+        'Полицейский',
+        'Военный',
+        'Хирург',
+        'Медсестра',
+        'Химик',
+        'Физик',
+        'Астро-физик'])
+
+player = Сharacter()
