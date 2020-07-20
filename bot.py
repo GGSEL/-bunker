3f6b3c42b5444ad04eba9929f056a1caf8c352d8
Alexander <sashakaradyaur345@gmail.com>
2020-07-20 20:33:51 +0300

    Added function 'showrole' and fixed some bugs

--- bot.py
+++ bot.py
@@ -2,18 +2,23 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
+import config
 
 client = commands.Bot(command_prefix='!')
 
-channels.id = {
-    'news': iD,
-}
-
+# channels.id = {
+#     'news': iD,
+# }
 
 @client.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def clear(ctx, amount=100):
     await ctx.channel.purge(limit=amount)
 
+@client.command(pass_context=True)
+async def showrole(ctx):
+    
+    await ctx.send(config.persone.showrole())
 
-client.run('token')
+
+client.run('NzM0Nzk2ODk3MTU5NzQxNTQw.XxXLVA.DkIcRM7W5LGfLZrp6dTBrhDUoE4')
