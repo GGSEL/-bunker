28283560a80aa22e8ab48fd7d3ceadaa8e9c10c9
Alexander <sashakaradyaur345@gmail.com>
2020-07-21 16:39:23 +0300

    New function !play

--- bot.py
+++ bot.py
@@ -2,34 +2,50 @@ import discord
 from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Bot
+from discord.utils import get
 import asyncio
 import character
 
-client = commands.Bot(command_prefix='!')
+
+bot = commands.Bot(command_prefix='!')
 
 # channels.id = {
 #     'news': iD,
 # }
 
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def clear(ctx, amount=100):
     await ctx.channel.purge(limit=amount)
 
 
-@client.command(pass_context=True)
+@bot.event
+async def on_message(message):
+    await bot.process_commands( message )
+
+    if message.content.startswith('**Игра начинается!**'):
+        await message.add_reaction('☄️')
+
+
+@bot.command(pass_context=True)
+async def play(message):
+
+    await message.channel.send('**Игра начинается!**')
+
+
+@bot.command(pass_context=True)
 async def showrole(ctx, *, quontity):
     await ctx.channel.purge(limit=1)
-    embed = discord.Embed(title = 'Тебе выдана роль!',
-                          colour = discord.Color.green())
-    embed.set_author(name = client.user.name, icon_url = client.user.avatar_url)
+    embed = discord.Embed(title='Тебе выдана роль!',
+                          colour=discord.Color.green())
+    embed.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
     quontity = int(quontity)
 
     for i in range(quontity):
         c = character.create_Character(quontity)
-        embed.set_footer(text = c)
-        await ctx.send(embed = embed)
+        embed.set_footer(text=c)
+        await ctx.send(embed=embed)
 
 
-client.run('NzM0Nzk2ODk3MTU5NzQxNTQw.XxXLVA.DkIcRM7W5LGfLZrp6dTBrhDUoE4')
+bot.run('NzM0Nzk2ODk3MTU5NzQxNTQw.XxXLVA.DkIcRM7W5LGfLZrp6dTBrhDUoE4')
