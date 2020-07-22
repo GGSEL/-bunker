c8cc1be3c7551aadfc9823f8618e58ff88670025
Alexander <sashakaradyaur345@gmail.com>
2020-07-22 17:07:48 +0300

    Fixed bugs

--- bot.py
+++ bot.py
@@ -25,8 +25,9 @@ async def clear(ctx, amount=100):
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
+
     if message.content.startswith('**Игра начинается!**') and message.author.id == 734796897159741540:
-        await message.add_reaction('☄️')
+        await message.add_reaction('cometa_746623:735442759057539092')
 
 
 @bot.command(pass_context=True)
@@ -39,7 +40,7 @@ async def play(message):
 
 @bot.event
 async def on_raw_reaction_add(payload):
-    if game == True:
+    if payload.emoji.id == '735442759057539092':
         print('success')
         member = payload.member
         embed = discord.Embed(title='Тебе выдана роль!',
@@ -50,18 +51,18 @@ async def on_raw_reaction_add(payload):
         await member.send(embed=embed)
 
 
-@bot.command(pass_context=True)
-async def showrole(ctx, *, quontity):
-    await ctx.channel.purge(limit=1)
-    embed = discord.Embed(title='Тебе выдана роль!',
-                          colour=discord.Color.green())
-    embed.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
-    quontity = int(quontity)
+# @bot.command(pass_context=True)
+# async def showrole(ctx, *, quontity):
+#     await ctx.channel.purge(limit=1)
+#     embed = discord.Embed(title='Тебе выдана роль!',
+#                           colour=discord.Color.green())
+#     embed.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
+#     quontity = int(quontity)
 
-    for i in range(quontity):
-        c = character.create_Character(quontity)
-        embed.set_footer(text=c)
-        await ctx.send(embed=embed)
+#     for i in range(quontity):
+#         c = character.create_Character(quontity)
+#         embed.set_footer(text=c)
+#         await ctx.send(embed=embed)
 
 
 bot.run('NzM0Nzk2ODk3MTU5NzQxNTQw.XxXLVA.DkIcRM7W5LGfLZrp6dTBrhDUoE4')
